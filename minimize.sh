a3b50445ff09be0c223d05af626459af1a55bad2
Sergei Shulepov <s.pepyakin@gmail.com>
2025-11-29 20:09:55 +0100

    minimum repro

---
+++ minimize.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Script for finding a minimum repro.
+
+N=1
+while true; do
+    echo "Testing N=$N"
+    RUST_BACKTRACE=1 RUST_LOG=info \
+        ./target/release/rollup_emulator \
+        --number-of-blocks=20 \
+        --fast-sequencers=1 \
+        --sleepy-sequencers=0 \
+        --storage-path=/tmp \
+        --seed "$N"
+
+    status=$?
+    if [ $status -ne 0 ]; then
+        say done;
+        echo "❌ Failed at N=$N (exit code $status)"
+        exit 1
+    fi
+
+    N=$((N+1))
+done
